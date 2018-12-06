acfd314889384d06315f46121f9246732a3e86dc
Padraig Gleeson <p.gleeson@gmail.com>
2018-12-06 17:40:05 +0000

    Work in progress

---
+++ NeuroML2/active_cells/CreateCellModels.py
@@ -0,0 +1,78 @@
+import neuroml
+
+import neuroml.loaders as loaders
+import neuroml.writers as writers
+
+def create_active_cell(cell_id):
+
+    fn = '../%s.cell.nml'%cell_id
+    doc = loaders.NeuroMLLoader.load(fn)
+    print("Loaded morphology file from: "+fn)
+
+    cell = doc.cells[0]
+
+
+    channel_densities = []
+
+    cd_pas = neuroml.ChannelDensity(id="pas_chan", segment_groups="all", ion="non_specific", ion_channel="pas", erev="-70.0 mV", cond_density="0.021 mS_per_cm2")
+    channel_densities.append(cd_pas)
+
+
+
+    specific_capacitances = []
+
+    specific_capacitances.append(neuroml.SpecificCapacitance(value='1.0 uF_per_cm2',
+                                                segment_groups='all'))
+
+    init_memb_potentials = [neuroml.InitMembPotential(
+        value="-80 mV", segment_groups='all')]
+
+    membrane_properties = neuroml.MembraneProperties(
+        channel_densities=channel_densities,
+        specific_capacitances=specific_capacitances,
+        init_memb_potentials=init_memb_potentials)
+
+    # Intracellular Properties
+    #
+    resistivities = []
+    resistivities.append(neuroml.Resistivity(
+        value="100 ohm_cm", segment_groups='all'))
+
+    intracellular_properties = neuroml.IntracellularProperties(resistivities=resistivities)
+
+    bp = neuroml.BiophysicalProperties(id="biophys",
+                                       intracellular_properties=intracellular_properties,
+                                       membrane_properties=membrane_properties)
+
+    cell.biophysical_properties = bp
+
+    cell.id = '%s'%cell_id
+
+    nml_doc2 = neuroml.NeuroMLDocument(id=cell.id)
+
+    nml_doc2.includes.append(neuroml.IncludeType('pas.channel.nml')) 
+    #nml_doc2.includes.append(neuroml.IncludeType('channelConvert/Na_BC.channel.nml')) 
+    #nml_doc2.includes.append(neuroml.IncludeType('channelConvert/K_BC.channel.nml')) 
+    nml_doc2.cells.append(cell)
+
+    nml_file = cell.id+'_active.cell.nml'
+
+    writers.NeuroMLWriter.write(nml_doc2,nml_file)
+
+    print("Saved modified morphology file to: "+nml_file)
+
+
+    ###### Validate the NeuroML ######    
+
+    from neuroml.utils import validate_neuroml2
+
+    validate_neuroml2(nml_file)
+
+
+if __name__ == "__main__":
+    
+    cells = {'AA0173'}
+    for cell_id in cells:
+        create_active_cell(cell_id)
+    
+    
---
+++ NeuroML2/active_cells/CreateNetwork.py
@@ -0,0 +1,102 @@
+
+import neuroml
+
+import neuroml.loaders as loaders
+import neuroml.writers as writers
+
+from pyneuroml.lems import generate_lems_file_for_neuroml
+
+def create_network_for_cell(cell_id):
+
+    net_ref = "Net_%s"%cell_id
+    net_doc = neuroml.NeuroMLDocument(id=net_ref)
+
+    net = neuroml.Network(id=net_ref)
+    net_doc.networks.append(net)
+
+
+    net_doc.includes.append(neuroml.IncludeType(cell_id+'_active.cell.nml')) 
+
+    pop = neuroml.Population(id="Pop_%s"%cell_id,
+                component=cell_id,
+                type="populationList")
+
+    inst = neuroml.Instance(id="0")
+    pop.instances.append(inst)
+    inst.location = neuroml.Location(x=0, y=0, z=0)
+    net.populations.append(pop)
+
+    stim = neuroml.PulseGenerator(id='stim0',
+                                 delay='50ms',
+                                 duration='200ms',
+                                 amplitude='0.5nA')
+
+    net_doc.pulse_generators.append(stim)
+
+
+    input_list = neuroml.InputList(id="%s_input"%stim.id,
+                                   component=stim.id,
+                                   populations=pop.id)
+
+    syn_input = neuroml.Input(id=0,
+                              target="../%s/0/%s" % (pop.id, pop.component),
+                              destination="synapses")
+
+    input_list.input.append(syn_input)
+    net.input_lists.append(input_list)
+
+
+
+    nml_file = net.id+'.net.nml'
+
+    writers.NeuroMLWriter.write(net_doc,nml_file)
+
+    print("Saved network file to: "+nml_file)
+
+
+    ###### Validate the NeuroML ######    
+
+    from neuroml.utils import validate_neuroml2
+
+    validate_neuroml2(nml_file)
+
+    sim_id = 'Test_%s'%cell_id
+    target = net.id
+    duration=400
+    dt = 0.025
+    lems_file_name = 'LEMS_%s.xml'%sim_id
+    target_dir = "."
+
+    interesting_seg_ids = [0,1002,1000061]
+
+    to_plot = {'Some_voltages':[]}
+    to_save = {'%s_voltages.dat'%cell_id:[]}
+
+    for seg_id in interesting_seg_ids:
+        to_plot.values()[0].append('%s/0/%s/%s/v'%(pop.id, pop.component,seg_id))
+        to_save.values()[0].append('%s/0/%s/%s/v'%(pop.id, pop.component,seg_id))
+
+    generate_lems_file_for_neuroml(sim_id, 
+                                   nml_file, 
+                                   target, 
+                                   duration, 
+                                   dt, 
+                                   lems_file_name,
+                                   target_dir,
+                                   gen_plots_for_all_v = False,
+                                   plot_all_segments = False,
+                                   gen_plots_for_quantities = to_plot,   #  Dict with displays vs lists of quantity paths
+                                   gen_saves_for_all_v = False,
+                                   save_all_segments = False,
+                                   gen_saves_for_quantities = to_save,   #  Dict with file names vs lists of quantity paths
+                                   copy_neuroml = False)
+
+
+
+if __name__ == "__main__":
+    
+    cells = {'AA0173'}
+    for cell_id in cells:
+        create_network_for_cell(cell_id)
+
+
